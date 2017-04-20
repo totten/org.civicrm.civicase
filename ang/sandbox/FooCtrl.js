cee6430c0908ba24ec0f8080973691f025d6cb9d
Tim Otten <totten@civicrm.org>
2017-04-20 00:35:23 -0700

    sandbox - add list ui

--- ang/sandbox/FooCtrl.js
+++ ang/sandbox/FooCtrl.js
@@ -1,6 +1,16 @@
 (function(angular, $, _) {
 
   angular.module('sandbox').config(function($routeProvider) {
+      $routeProvider.when('/sandbox', {
+        resolve: {
+          contacts: function(crmApi){return crmApi('Contact', 'get', {contact_type: 'Individual'});}
+        },
+        controller: function($scope, contacts) {
+          $scope.contacts = contacts;
+        },
+        template: '<ul><li ng-repeat="contact in contacts.values"><a ng-href="#/sandbox/{{contact.id}}/summary">{{contact.display_name}}</a></li></ul>'
+      });
+  
       $routeProvider.when('/sandbox/:id', {
         template: '<div></div>',
         controller: function($route, $location) {

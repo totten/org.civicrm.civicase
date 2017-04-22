b4c83716a2874629738364a9f1adb6a6735af77a
Tim Otten <totten@civicrm.org>
2017-04-21 21:50:24 -0700

    ListCtrl - Live bind URL, with support for back and editing

--- ang/sandbox/ListCtrl.js
+++ ang/sandbox/ListCtrl.js
@@ -1,17 +1,47 @@
-(function(angular, $, _) {
+(function (angular, $, _) {
 
-  angular.module('sandbox').config(function($routeProvider) {
+  angular.module('sandbox').factory('crmUiBindRoute', function ($location, $rootScope, $timeout, $route) {
+    var internalUpdate = false, activeTimer = null;
+    $rootScope.$on('$routeUpdate', function () {
+      // Only reload if someone else -- like the user or an <a href> -- changed URL.
+      if (!internalUpdate) {
+        $route.reload();
+      }
+    });
+
+    return function ($scope, scopeExpr, queryParam, queryDefaults) {
+      if (!queryDefaults) queryDefaults = {};
+      if ($location.search()[queryParam]) {
+        $scope[scopeExpr] = JSON.parse($location.search()[queryParam]);
+      }
+      else {
+        $scope[scopeExpr] = angular.extend({}, queryDefaults);
+      }
+
+      // Keep the URL bar up-to-date.
+      $scope.$watchCollection(scopeExpr, function (newFilters) {
+        internalUpdate = true;
+        $location.search(queryParam, JSON.stringify(newFilters));
+        if (activeTimer) $timeout.cancel(activeTimer);
+        activeTimer = $timeout(function () {
+          internalUpdate = false;
+          activeTimer = null;
+        }, 50);
+      });
+    };
+  });
+
+  angular.module('sandbox').config(function ($routeProvider) {
       $routeProvider.when('/sandbox', {
-        resolve: {
-          contacts: function(crmApi){return crmApi('Contact', 'get', {contact_type: 'Individual'});}
-        },
-        controller: function($scope, contacts) {
-          $scope.contacts = contacts;
+        reloadOnSearch: false,
+        controller: function ($scope, crmUiBindRoute) {
+          crmUiBindRoute($scope, 'contactFilters', 'contacts', {
+            contact_type: 'Individual'
+          });
         },
         templateUrl: '~/sandbox/ListCtrl.html'
       });
     }
   );
-
 
 })(angular, CRM.$, CRM._);
